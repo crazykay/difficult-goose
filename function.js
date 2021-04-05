80b90ecce994f2076a9761f0b9e7267a3e058283
ck <kai64286513@gmail.com>
2021-04-06 03:01:15 +0800

    test static html

--- function.js
+++ function.js
@@ -38,36 +38,38 @@ async function handleRequest(request) {
       headers: { "content-type": "text/html" },
     });
   }
-  const radioUrl = new URL('radio.html', import.meta.url);
-  const result = await fetch(radioUrl);
-  return new Response(result,{
-    headers: {
-      "content-type": "text/html; charset=UTF-8",
+
+  if (pathname.startsWith('/radio')) {
+    const radioUrl = new URL('radio.html', import.meta.url);
+    return await fetch(radioUrl);
+  }
+
+  return new Response(
+    `<body
+      align="center"
+      style="font-family: Avenir, Helvetica, Arial, sans-serif; font-size: 1.5rem;"
+    >
+      <h1>Return JSON and/or HTML Example</h1>
+      <h2>goose!!</h2>
+      <p>
+        <a href="/html">/html</a> - responds with HTML to the request.
+      </p>
+      <p>
+        <a href="/json">/json</a> - responds with JSON to the request.
+      </p>
+      <p>
+        <a href="/radio">/radio</a> - radio
+      </p>
+      <p>
+        <a href="/ip">/ip</a> - responds client ip.
+      </p>
+    </body>`,
+    {
+      headers: {
+        "content-type": "text/html; charset=UTF-8",
+      },
     },
-  });
-//   return new Response(
-//     `<body
-//       align="center"
-//       style="font-family: Avenir, Helvetica, Arial, sans-serif; font-size: 1.5rem;"
-//     >
-//       <h1>Return JSON and/or HTML Example</h1>
-//       <h2>goose!!</h2>
-//       <p>
-//         <a href="/html">/html</a> - responds with HTML to the request.
-//       </p>
-//       <p>
-//         <a href="/json">/json</a> - responds with JSON to the request.
-//       </p>
-//       <p>
-//         <a href="/ip">/ip</a> - responds client ip.
-//       </p>
-//     </body>`,
-//     {
-//       headers: {
-//         "content-type": "text/html; charset=UTF-8",
-//       },
-//     },
-//   );
+  );
 }
 
 addEventListener("fetch", (event) => {
